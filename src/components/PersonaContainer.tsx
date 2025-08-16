409ab40521d53fcfed86d6c43c069c5d5c500a3c
Kalph upadhyay <kalpupadhyay@gmail.com>
2025-08-16 13:09:45 +0530

    feat: update PersonaContainer and Header components for improved styling and responsiveness; add Gemini SVG asset

--- src/components/PersonaContainer.tsx
+++ src/components/PersonaContainer.tsx
@@ -7,15 +7,24 @@ export const PersonaContainer: React.FC<{
   return (
     <div
       onClick={personaClick}
-      className="persona-card p-4 border rounded-lg shadow-md flex cursor-pointer hover:shadow-lg transition-shadow duration-200"
+      className="persona-card p-5 border border-gray-300 rounded-lg shadow-md flex cursor-pointer 
+      hover:shadow-xl transition-all duration-300 hover:border-blue-400 
+      hover:bg-gradient-to-br 
+      hover:-translate-y-1 hover:scale-105 transform"
     >
-      <div className="img size-28 mr-4 rounded-lg overflow-auto">
-        <img src={image} alt={name} className="w-full h-full" />
+      <div className="img size-28 mr-4 rounded-lg overflow-hidden">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full  transition-transform duration-300 group-hover:scale-110"
+        />
       </div>
-      <div className="">
-        <h2 className="text-xl font-semibold ">{name}</h2>
-        <div className="flex items-center gap-2">
-          <div className="size-2.5 bg-green-500 rounded-full"></div>
+      <div className="transition-all duration-300">
+        <h2 className="text-xl font-semibold hover:text-blue-600 transition-colors duration-300">
+          {name}
+        </h2>
+        <div className="flex items-center gap-2 mb-2">
+          <div className="size-2.5 bg-green-500 rounded-full animate-pulse"></div>
           <p>Online</p>
         </div>
         <p className="text-gray-700">{description}</p>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,11 @@ export const Header = () => {
       onClick={() => navigate("/")}
       className="bg-gray-800 text-white p-4 flex justify-between items-center cursor-pointer"
     >
-      <h1 className="text-2xl font-bold">Persona AI</h1>
-      <p>Powered by </p>
+      <h1 className="text-2xl font-bold">Chat with Kalph</h1>
+      <div className="flex items-center gap-2">
+        <p>Powered by Google Gemini </p>
+        <img src="/gemini-color.svg" height={20} width={20} alt="" />
+      </div>
     </header>
   );
 };
